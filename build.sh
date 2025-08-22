ce0a8eb778f94df7dcdb7d36941f428f3192ae66
anik0000000 <idozmor@gmail.com>
2025-08-22 16:06:39 +0500

    Step 3 fix fix fix fix fix fix fix fix fix fix fix

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 # Установка зависимостей
-make install
-make collectstatic
-make migrate
+uv sync
+
+# Применяем миграции (КРИТИЧЕСКИ ВАЖНО!)
+uv run python manage.py migrate
+
+# Сборка статики
+uv run python manage.py collectstatic --noinput
